c9ae536989ccffe7e4f502fb99175f1c85d76214
Arthur <chaff.arthur@gmail.com>
2024-01-03 23:22:58 +0100

    fix eslint errors

--- src/components/app/ChessBoard.vue
+++ src/components/app/ChessBoard.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="chess-board" ref="chessBoardElement" @drop="dropPiece" @dragover.prevent>
-        <img class="piece" :class="piece.playerId === playerId ? 'cursor-grab' : 'cursor-default'" @click="selectPiece(piece)" @drag="pieceDragging($event, piece)" @dragstart="pieceDragStart" v-for="piece in boardPieces"
-        :style="[
-            { 'transform': `translate(${piece.colIndex * 100}%, ${piece.rowIndex * 100}%)` }
-        ]" :src="getImageUrl(piece.name)" />
-        <div v-for="(_, colIndex) in boardSquares" class="column">
-            <div v-for="(_, rowIndex) in boardSquares" :key="`${colIndex}-${rowIndex}`" class="cell" :class="[(colIndex + rowIndex) % 2 === 0 ? 'light' : 'dark', boardSquares[colIndex]![rowIndex]!.selected ? 'selected' : '']" @click="clickSquare(colIndex, rowIndex)"></div>
+        <img class="piece" :class="piece.playerId === playerId ? 'cursor-grab' : 'cursor-default'" @click="selectPiece(piece)" @drag="pieceDragging($event, piece)" @dragstart="pieceDragStart" v-for="piece in boardPieces" :key="`${piece.colIndex}-${piece.rowIndex}`"
+             :style="[
+                 { transform: `translate(${piece.colIndex * 100}%, ${piece.rowIndex * 100}%)` }
+             ]" :src="getImageUrl(piece.name)" />
+        <div v-for="(_1, colIndex) in boardSquares" :key="colIndex" class="column">
+            <div v-for="(_2, rowIndex) in boardSquares" :key="`${colIndex}-${rowIndex}`" class="cell" :class="[(colIndex + rowIndex) % 2 === 0 ? 'light' : 'dark', boardSquares[colIndex]![rowIndex]!.selected ? 'selected' : '']" @click="clickSquare(colIndex, rowIndex)"></div>
         </div>
     </div>
 </template>
--- src/composables/useChessBoard.ts
+++ src/composables/useChessBoard.ts
@@ -17,13 +17,13 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
         let rowIndex = Math.floor((mouseY - chessBoardElement.value!.offsetTop + window.scrollY) / cellSize.value);
 
         // top corner
-        if(mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) rowIndex = 0;
+        if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) rowIndex = 0;
         // right corner
-        if(mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) colIndex = 7;
+        if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) colIndex = 7;
         // bottom corner
-        if(mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) rowIndex = 7;
+        if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) rowIndex = 7;
         // left corner
-        if(mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) colIndex = 0;
+        if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) colIndex = 0;
 
         return { colIndex, rowIndex };
     };
@@ -33,21 +33,21 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
     };
 
     const dropPiece = (e: DragEvent | MouseEvent, type: 'drag' | 'mouse'): void => {
-        if(!isCurrentPlayer(selectedPiece.playerId)) return;
+        if (!isCurrentPlayer(selectedPiece.playerId)) return;
         draggingPiece.value = false;
         const mouseX = e.clientX;
         const mouseY = e.clientY;
         const { colIndex, rowIndex } = getCellFromMousePosition(mouseX, mouseY);
 
         let piece = null;
-        if(type === 'mouse') piece = (e as MouseEvent).relatedTarget as HTMLElement;
+        if (type === 'mouse') piece = (e as MouseEvent).relatedTarget as HTMLElement;
         else piece = (e as DragEvent).target as HTMLElement;
 
         piece.style.transform = `translate(${colIndex * 100}%, ${rowIndex * 100}%)`;
     };
 
     const mouseOver = (event: MouseEvent) => {
-        if(draggingPiece.value) dropPiece(event, 'mouse');
+        if (draggingPiece.value) dropPiece(event, 'mouse');
     };
 
     onMounted(() => {
@@ -108,21 +108,21 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
         ]),
         setPiecePosition(e: DragEvent, piece: BoardPieceInterface): void {
             Object.assign(selectedPiece, piece);
-            if(!isCurrentPlayer(piece.playerId)) return;
-            if(!draggingPiece.value) draggingPiece.value = true;
+            if (!isCurrentPlayer(piece.playerId)) return;
+            if (!draggingPiece.value) draggingPiece.value = true;
 
             let mouseX = e.clientX;
             let mouseY = e.clientY;
 
-            if(mouseX !== 0 && mouseY !== 0) {
+            if (mouseX !== 0 && mouseY !== 0) {
                 // top corner
-                if(mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) mouseY = chessBoardElement.value!.offsetTop - window.scrollY;
+                if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) mouseY = chessBoardElement.value!.offsetTop - window.scrollY;
                 // right corner
-                if(mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) mouseX = chessBoardSize.value + chessBoardElement.value!.offsetLeft - window.scrollX;
+                if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) mouseX = chessBoardSize.value + chessBoardElement.value!.offsetLeft - window.scrollX;
                 // bottom corner
-                if(mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) mouseY = chessBoardSize.value + chessBoardElement.value!.offsetTop - window.scrollY;
+                if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) mouseY = chessBoardSize.value + chessBoardElement.value!.offsetTop - window.scrollY;
                 // left corner
-                if(mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) mouseX = chessBoardElement.value!.offsetLeft - window.scrollX;
+                if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) mouseX = chessBoardElement.value!.offsetLeft - window.scrollX;
 
                 const translatePosX = 50 * ((mouseX - chessBoardElement.value!.offsetLeft + window.scrollX) / (cellSize.value / 2) - 1);
                 const translatePosY = 50 * ((mouseY - chessBoardElement.value!.offsetTop + window.scrollY) / (cellSize.value / 2) - 1);
@@ -142,11 +142,11 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
         },
         selectPiece(piece: BoardPieceInterface): void {
             // if no selected piece
-            if(!selectedPiece.name) {
+            if (!selectedPiece.name) {
                 const pieceToSelectIndex = this.getBoardPieceIndex(piece.colIndex, piece.rowIndex);
                 const pieceToSelect = this.boardPieces.value[pieceToSelectIndex];
 
-                if(isCurrentPlayer(pieceToSelect!.playerId)) {
+                if (isCurrentPlayer(pieceToSelect!.playerId)) {
                     this.boardSquares.value[piece.colIndex]![piece.rowIndex]!.selected = true;
 
                     Object.assign(selectedPiece, piece);
@@ -155,8 +155,8 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
                 this.boardSquares.value[selectedPiece.colIndex]![selectedPiece.rowIndex]!.selected = false;
 
                 // if not the same as current piece
-                if(piece.colIndex !== selectedPiece.colIndex || piece.rowIndex !== selectedPiece.rowIndex) {
-                    if(isCurrentPlayer(this.boardPieces.value[this.getBoardPieceIndex(piece.colIndex, piece.rowIndex)]!.playerId)) {
+                if (piece.colIndex !== selectedPiece.colIndex || piece.rowIndex !== selectedPiece.rowIndex) {
+                    if (isCurrentPlayer(this.boardPieces.value[this.getBoardPieceIndex(piece.colIndex, piece.rowIndex)]!.playerId)) {
                         this.boardSquares.value[piece.colIndex]![piece.rowIndex]!.selected = true;
 
                         Object.assign(selectedPiece, piece);
@@ -167,7 +167,7 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
             }
         },
         clickSquare(colIndex: number, rowIndex: number): void {
-            if(selectedPiece.name) {
+            if (selectedPiece.name) {
                 document.documentElement.style.setProperty('--transition', 'transform 0.15s ease-in-out');
                 const selectedPieceIndex = this.getBoardPieceIndex(selectedPiece.colIndex, selectedPiece.rowIndex);
                 this.boardSquares.value[selectedPiece.colIndex]![selectedPiece.rowIndex]!.selected = false;
