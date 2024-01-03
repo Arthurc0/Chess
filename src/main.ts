a9e2d04737d84ce1b6a61468d11eba622f29d3bf
Arthur <chaff.arthur@gmail.com>
2024-01-03 23:10:54 +0100

    remove useless parts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
 import '@/assets/style/main.css';
-
 import App from './App.vue';
 import router from './router';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
-
 app.mount('#app');
