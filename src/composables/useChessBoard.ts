fe7ae6b1529da90a7e3017b1d1b42c0c305a60f6
Arthur <chaff.arthur@gmail.com>
2024-01-09 21:15:39 +0100

    move composable methods

--- src/composables/useChessBoard.ts
+++ src/composables/useChessBoard.ts
@@ -12,26 +12,6 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
     const chessBoardSize = computed(() => chessBoardElement.value!.clientWidth);
     const draggingPiece = ref(false);
 
-    const getCellFromMousePosition = (mouseX: number, mouseY: number): { colIndex: number; rowIndex: number } => {
-        let colIndex = Math.floor((mouseX - chessBoardElement.value!.offsetLeft + window.scrollX) / cellSize.value);
-        let rowIndex = Math.floor((mouseY - chessBoardElement.value!.offsetTop + window.scrollY) / cellSize.value);
-
-        // top corner
-        if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) rowIndex = 0;
-        // right corner
-        if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) colIndex = 7;
-        // bottom corner
-        if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) rowIndex = 7;
-        // left corner
-        if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) colIndex = 0;
-
-        return { colIndex, rowIndex };
-    };
-
-    const isCurrentPlayer = (playerId: number): boolean => {
-        return playerId === currentPlayerId.value;
-    };
-
     const boardSquares = ref<BoardSquareInterface[][]>([
         [{ name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }],
         [{ name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }],
@@ -42,16 +22,6 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
         [{ name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }],
         [{ name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }, { name: '' }]
     ]);
-
-    const unselectPiece = () => {
-        if (selectedPiece.name) boardSquares.value[selectedPiece.colIndex]![selectedPiece.rowIndex]!.selected = false;
-        Object.assign(selectedPiece, { colIndex: -1, name: '', rowIndex: -1 });
-    };
-
-    const setSelectedPiece = (piece: BoardPieceInterface) => {
-        if (isCurrentPlayer(piece.playerId)) boardSquares.value[piece.colIndex]![piece.rowIndex]!.selected = true;
-        Object.assign(selectedPiece, piece);
-    };
 
     const boardPieces = ref<BoardPieceInterface[]>([
         { colIndex: 0, name: 'b-rook', playerId: 0, rowIndex: 0 },
@@ -87,6 +57,36 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
         { colIndex: 6, name: 'w-knight', playerId: 1, rowIndex: 7 },
         { colIndex: 7, name: 'w-rook', playerId: 1, rowIndex: 7 }
     ]);
+
+    const getCellFromMousePosition = (mouseX: number, mouseY: number): { colIndex: number; rowIndex: number } => {
+        let colIndex = Math.floor((mouseX - chessBoardElement.value!.offsetLeft + window.scrollX) / cellSize.value);
+        let rowIndex = Math.floor((mouseY - chessBoardElement.value!.offsetTop + window.scrollY) / cellSize.value);
+
+        // top corner
+        if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) rowIndex = 0;
+        // right corner
+        if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) colIndex = 7;
+        // bottom corner
+        if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) rowIndex = 7;
+        // left corner
+        if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) colIndex = 0;
+
+        return { colIndex, rowIndex };
+    };
+
+    const isCurrentPlayer = (playerId: number): boolean => {
+        return playerId === currentPlayerId.value;
+    };
+
+    const unselectPiece = () => {
+        if (selectedPiece.name) boardSquares.value[selectedPiece.colIndex]![selectedPiece.rowIndex]!.selected = false;
+        Object.assign(selectedPiece, { colIndex: -1, name: '', rowIndex: -1 });
+    };
+
+    const setSelectedPiece = (piece: BoardPieceInterface) => {
+        if (isCurrentPlayer(piece.playerId)) boardSquares.value[piece.colIndex]![piece.rowIndex]!.selected = true;
+        Object.assign(selectedPiece, piece);
+    };
 
     const getBoardPieceIndex = (colIndex: number, rowIndex: number): number => {
         return boardPieces.value.findIndex((p) => p.colIndex === colIndex && p.rowIndex === rowIndex);
@@ -125,74 +125,83 @@ export const useChessBoard = (chessBoardElement: Ref<HTMLElement | undefined>) =
             mouseOver(event);
         });
     });
+
     onUnmounted(() => {
         document.removeEventListener('mouseover', (event: MouseEvent) => {
             mouseOver(event);
         });
     });
 
-    return {
-        boardSquares,
-        boardPieces,
-        movePiece(e: DragEvent, piece: BoardPieceInterface): void {
-            if (!isCurrentPlayer(piece.playerId)) return;
-            if (!draggingPiece.value) draggingPiece.value = true;
+    const movePiece = (e: DragEvent, piece: BoardPieceInterface): void => {
+        if (!isCurrentPlayer(piece.playerId)) return;
+        if (!draggingPiece.value) draggingPiece.value = true;
 
-            let mouseX = e.clientX;
-            let mouseY = e.clientY;
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
 
-            if (mouseX !== 0 && mouseY !== 0) {
-                // top corner
-                if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) mouseY = chessBoardElement.value!.offsetTop - window.scrollY;
-                // right corner
-                if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) mouseX = chessBoardSize.value + chessBoardElement.value!.offsetLeft - window.scrollX;
-                // bottom corner
-                if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) mouseY = chessBoardSize.value + chessBoardElement.value!.offsetTop - window.scrollY;
-                // left corner
-                if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) mouseX = chessBoardElement.value!.offsetLeft - window.scrollX;
+        if (mouseX !== 0 && mouseY !== 0) {
+            // top corner
+            if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY <= 0) mouseY = chessBoardElement.value!.offsetTop - window.scrollY;
+            // right corner
+            if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX >= chessBoardSize.value) mouseX = chessBoardSize.value + chessBoardElement.value!.offsetLeft - window.scrollX;
+            // bottom corner
+            if (mouseY - chessBoardElement.value!.offsetTop + window.scrollY >= chessBoardSize.value) mouseY = chessBoardSize.value + chessBoardElement.value!.offsetTop - window.scrollY;
+            // left corner
+            if (mouseX - chessBoardElement.value!.offsetLeft + window.scrollX <= 0) mouseX = chessBoardElement.value!.offsetLeft - window.scrollX;
 
-                const translatePosX = 50 * ((mouseX - chessBoardElement.value!.offsetLeft + window.scrollX) / (cellSize.value / 2) - 1);
-                const translatePosY = 50 * ((mouseY - chessBoardElement.value!.offsetTop + window.scrollY) / (cellSize.value / 2) - 1);
+            const translatePosX = 50 * ((mouseX - chessBoardElement.value!.offsetLeft + window.scrollX) / (cellSize.value / 2) - 1);
+            const translatePosY = 50 * ((mouseY - chessBoardElement.value!.offsetTop + window.scrollY) / (cellSize.value / 2) - 1);
 
-                (e.target as HTMLElement).style.transform = `translate(${translatePosX}%, ${translatePosY}%)`;
+            (e.target as HTMLElement).style.transform = `translate(${translatePosX}%, ${translatePosY}%)`;
+        }
+    };
+
+    const pieceDragStyle = (e: DragEvent, piece: BoardPieceInterface): void => {
+        e.dataTransfer!.setDragImage(e.target as HTMLElement, -9999, -9999);
+        e.dataTransfer!.effectAllowed = 'move';
+        unselectPiece();
+        setSelectedPiece(piece);
+        if (!isCurrentPlayer(piece.playerId)) return;
+        document.documentElement.style.setProperty('--transition', 'none');
+        (e.target as HTMLElement).style.zIndex = `${pieceMovesCount.value++}`;
+    };
+
+    const selectPiece = (piece: BoardPieceInterface): void => {
+        // if no selected piece
+        if (!selectedPiece.name) {
+            if (isCurrentPlayer(piece.playerId)) {
+                setSelectedPiece(piece);
             }
-        },
-        pieceDragStyle(e: DragEvent, piece: BoardPieceInterface): void {
-            e.dataTransfer!.setDragImage(e.target as HTMLElement, -9999, -9999);
-            e.dataTransfer!.effectAllowed = 'move';
-            unselectPiece();
-            setSelectedPiece(piece);
-            if (!isCurrentPlayer(piece.playerId)) return;
-            document.documentElement.style.setProperty('--transition', 'none');
-            (e.target as HTMLElement).style.zIndex = `${pieceMovesCount.value++}`;
-        },
-        dropPiece,
-        getBoardPieceIndex,
-        selectPiece(piece: BoardPieceInterface): void {
-            // if no selected piece
-            if (!selectedPiece.name) {
-                if (isCurrentPlayer(piece.playerId)) {
+        } else {
+            boardSquares.value[selectedPiece.colIndex]![selectedPiece.rowIndex]!.selected = false;
+
+            // if not the same as current piece
+            if (piece.colIndex !== selectedPiece.colIndex || piece.rowIndex !== selectedPiece.rowIndex) {
+                if (isCurrentPlayer(boardPieces.value[getBoardPieceIndex(piece.colIndex, piece.rowIndex)]!.playerId)) {
                     setSelectedPiece(piece);
-                }
+                } else unselectPiece();
             } else {
-                this.boardSquares.value[selectedPiece.colIndex]![selectedPiece.rowIndex]!.selected = false;
-
-                // if not the same as current piece
-                if (piece.colIndex !== selectedPiece.colIndex || piece.rowIndex !== selectedPiece.rowIndex) {
-                    if (isCurrentPlayer(this.boardPieces.value[this.getBoardPieceIndex(piece.colIndex, piece.rowIndex)]!.playerId)) {
-                        setSelectedPiece(piece);
-                    } else unselectPiece();
-                } else {
-                    unselectPiece();
-                }
-            }
-        },
-        clickSquare(colIndex: number, rowIndex: number): void {
-            if (selectedPiece.name && isCurrentPlayer(selectedPiece.playerId)) {
-                document.documentElement.style.setProperty('--transition', 'transform 0.15s ease-in-out');
-                placeSelectedPiece(colIndex, rowIndex);
                 unselectPiece();
             }
         }
+    };
+
+    const clickSquare = (colIndex: number, rowIndex: number): void => {
+        if (selectedPiece.name && isCurrentPlayer(selectedPiece.playerId)) {
+            document.documentElement.style.setProperty('--transition', 'transform 0.15s ease-in-out');
+            placeSelectedPiece(colIndex, rowIndex);
+            unselectPiece();
+        }
+    };
+
+    return {
+        boardSquares,
+        boardPieces,
+        movePiece,
+        pieceDragStyle,
+        dropPiece,
+        getBoardPieceIndex,
+        selectPiece,
+        clickSquare
     };
 };
